d41becce25dd6e4e3a15f05288a2d3b985e86660
SprrowZ <33713300+SprrowZ@users.noreply.github.com>
2018-08-28 21:27:07 +0800

    提交

---
+++ app/src/main/java/com/rye/catcher/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rye.catcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rye.common.Common;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Common();
+    }
+}
---
+++ common/src/main/java/com/rye/common/Common.java
@@ -0,0 +1,7 @@
+package com.rye.common;
+
+/**
+ * Created by ZZG on 2018/8/28.
+ */
+public class Common {
+}
